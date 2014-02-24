ed28490afcf63a266ab146438e30b27fa3c5ad6f
Supanat Auengsirirat <joong555@Supanats-MacBook-Pro-2.local>
2014-02-24 20:33:47 +0700

    Change settins.py to have engine dblite3, add database path and name as gofishdb, create model in models.py'

--- go_fish_project/go_fish_project/settings.py
+++ go_fish_project/go_fish_project/settings.py
@@ -6,6 +6,7 @@ PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+DATABASE_PATH = os.path.join(PROJECT_PATH, 'gofish.db')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -18,8 +19,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_PATH,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
--- go_fish_project/gofish/models.py
+++ go_fish_project/gofish/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
 # Create your models here.
+
+class Fish(models.Model):
+	name = models.CharField(max_length=128)
+	rarity = models.CharField(max_length=10)
+	sell_price = models.DecimalField(max_digits=9, decimal_places=2)
+	class Meta:
+		unique_together = ("name","rarity")	
+	
+	def __unicode__(self):
+		return "name:"+self.name+" rarity:"+self.rarity+" price:"+str(self.sell_price)
+
+
+class Item(models.Model):
+	name = models.CharField(max_length=128,unique=True)
+	rarity = models.CharField(max_length=10)
+	sell_price = models.DecimalField(max_digits=9, decimal_places=2)	
+	description = models.CharField(max_length=128)
+	
+#inherit Item
+class Fishhook(Item):
+	quality = models.IntegerField()
+	def __unicode__(self):
+                return "name:"+self.name+" rarity:"+self.rarity+" price:"+str(self.sell_price)
+
+class Fishingboat(Item):
+	quality = models.IntegerField()
+        def __unicode__(self):
+                return "name:"+self.name+" rarity:"+self.rarity+" price:"+str(self.sell_price)
+
+class Bait(Item):
+	quality = models.IntegerField()
+	def __unicode__(self):
+                return "name:"+self.name+" rarity:"+self.rarity+" price:"+str(self.sell_price)
+
+class Place(models.Model):
+	name = models.CharField(max_length=128,unique=True)
+	rarity = models.CharField(max_length=10)
+	level_need = models.IntegerField()
+	boatquality_need = models.IntegerField()
+	fish = models.ManyToManyField(Fish)
+
+	def __unicode__(self):
+                return "name:"+self.name+" rarity:"+self.rarity
+
+class Trophy(models.Model):
+	type = models.CharField(max_length=10)
+	def __unicode__(self):
+                return "type: "+self.type
+
+class User(models.Model):
+	username = models.CharField(max_length = 10,unique=True)
+	password = models.CharField(max_length = 10,unique=True)
+	name = models.CharField(max_length = 10,unique=True)
+	rank = models.IntegerField()
+	level = models.IntegerField()
+	money = models.DecimalField(max_digits=12, decimal_places=2)
+	trophy = models.ManyToManyField(Trophy, through='Containtrophy')
+        fishhook = models.ManyToManyField(Fishhook)
+	fishingboat = models.ManyToManyField(Fishingboat)
+	bait = models.ManyToManyField(Bait, through='Containbait')
+	fish = models.ManyToManyField(Fish, through='Containfish')
+        def __unicode__(self):
+		return "name: "+self.name+" username: "+self.username+"level: "+str(self.level)
+
+class Shop(models.Model):
+	name = models.CharField(max_length = 20)
+	fishhook = models.ManyToManyField(Fishhook)
+        fishingboat = models.ManyToManyField(Fishingboat)
+        bait = models.ManyToManyField(Bait)
+	def __unicode__(self):
+		return "name: "+self.name
+
+class Containfish(models.Model):
+	fish = models.ForeignKey(Fish)
+	user = models.ForeignKey(User)
+	amount = models.IntegerField()
+	def __unicode__(self):
+		return "USER:"+self.user.name+"//Contain:"+self.fish.name+"//Amount:"+str(self.amount)
+
+class Containbait(models.Model):
+	bait = models.ForeignKey(Bait)
+	user = models.ForeignKey(User)
+	amount = models.IntegerField()
+	def __unicode__(self):
+                return "USER:"+self.user.name+"//Contain:"+self.bait.name+"//Amount:"+str(self.amount)
+
+class Containtrophy(models.Model):
+	trophy = models.ForeignKey(Trophy)
+	user = models.ForeignKey(User)
+	amount = models.IntegerField()
+        def __unicode__(self):
+                return "USER:"+self.user.name+"//Contain:"+self.trophy.type+"//Amount:"+str(self.amount)
+ 
+
