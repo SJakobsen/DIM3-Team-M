dfa83be3e7b8e50498b19821c8905b655c4605ba
Alexander Shidlovsky <shidlovsky.alexander@gmail.com>
2014-03-16 16:29:02 +0000

    FIX: moved weather generator to the generators package

--- go_fish_project/gofish/generators.py
+++ go_fish_project/gofish/generators.py
@@ -1,5 +1,6 @@
 from random import randint
 from math import floor
+from random import gauss
 
 worldWidth = 20
 worldHeight = 16
@@ -14,3 +15,31 @@ def generateLake():
     		lake[i].append(floor(randint(1,10)))
 
     return lake
+
+def generateWeather():
+	weatherSet = []
+	for i in range(12):
+		weatherList = {}
+		rain = 0
+		windms = -1
+		winddir = -1
+		temp = -1
+		
+		clouds = randint(0,2)
+		weatherList['clouds'] = clouds
+		
+		if clouds != 0:
+			rain = randint(1,2)
+		weatherList['rain'] = rain
+		
+		windms = gauss(8+rain+clouds,2+rain)
+		weatherList['windms'] = windms
+		
+		winddir = randint(0,3)
+		weatherList['winddir'] = winddir
+		
+		temp = randint(10,25)
+		weatherList['temp'] = temp
+
+		weatherSet.append(dict(weatherList))
+	return weatherSet    
