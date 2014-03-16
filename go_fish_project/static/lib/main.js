119e4f725523981c590c83dd22e7c7405bc5054a
Alexander Shidlovsky <shidlovsky.alexander@gmail.com>
2014-03-16 19:31:51 +0000

    FEATURE: changed fishing results

--- go_fish_project/static/lib/main.js
+++ go_fish_project/static/lib/main.js
@@ -323,8 +323,24 @@ initUI = function() {
 };
 
 showFishingResults = function(result) {
-  var res;
+  var res, x, _i, _len, _ref;
   res = JSON.stringify(result);
+  if (result.fish.length > 0) {
+    if (result.fish.length > 1) {
+      res = "Yay! You have caught some fishes! Take a look at them!";
+    } else {
+      res = "Yay! You have caught a fish! Take a look at it!";
+    }
+    res += "<ul>";
+    _ref = result.fish;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      x = _ref[_i];
+      res += "<li>" + x.fish + " has price " + x.price + ", weight: " + x.weight + " and size: " + x.size + "</li>";
+    }
+    res += "</ul>";
+  } else {
+    res = "Sorry, no was fish caught :(";
+  }
   return $("#fishing-result").html(res);
 };
 
--- go_fish_project/static/src/ui.coffee
+++ go_fish_project/static/src/ui.coffee
@@ -5,6 +5,21 @@ initUI = () ->
 
 showFishingResults = (result) ->
 	res = JSON.stringify result
+	
+	if result.fish.length > 0
+		if result.fish.length > 1
+			res = "Yay! You have caught some fishes! Take a look at them!"
+
+		else 
+			res = "Yay! You have caught a fish! Take a look at it!"
+
+		res += "<ul>"
+		for x in result.fish
+			res += "<li>"+x.fish+" has price "+x.price+", weight: "+x.weight+" and size: "+x.size+"</li>"
+		res += "</ul>"
+	else
+		res = "Sorry, no was fish caught :("
+
 	$("#fishing-result").html(res);
 
 waitUI = () ->
