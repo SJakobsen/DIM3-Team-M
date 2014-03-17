f8f86ccf1d4737cf5a707fc1e4b888582c1c0378
Gabriel A Susanu <2021928s@bo720-7-06.(none)>
2014-03-17 20:42:45 +0000

    Trophy list fix

--- go_fish_project/gofish/views.py
+++ go_fish_project/gofish/views.py
@@ -411,6 +411,10 @@ def finish(request):
                 trophy.price = f.price
                 trophy.save()
                 dTrophy = {'name': f.fish.name, 'size': f.size, 'weight': float(f.weight), 'price': f.price}
+
+                for d in newTrophies:
+                    if d[name]=f.fish.name:
+                        newTrophies.remove(d)
                 newTrophies.append(dTrophy)
         except ObjectDoesNotExist:
             #No trophy of that fish type, add it
