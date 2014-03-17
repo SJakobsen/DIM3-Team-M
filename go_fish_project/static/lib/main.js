0ed53a771021225c7e9b409bcba411978de946a4
Alexander Shidlovsky <shidlovsky.alexander@gmail.com>
2014-03-17 13:01:30 +0000

    FIX: time representation

--- go_fish_project/static/lib/main.js
+++ go_fish_project/static/lib/main.js
@@ -6,7 +6,24 @@ print = function(str) {
 };
 
 getTimeString = function(time) {
-  return time;
+  var decimal, hours, minutes, str;
+  time += 6;
+  hours = Math.floor(time);
+  str = "";
+  if (time < 10) {
+    str += "0" + hours;
+  } else {
+    str += hours;
+  }
+  str += ":";
+  decimal = time % 1;
+  minutes = Math.floor(decimal * 60);
+  if (minutes < 10) {
+    str += "0" + minutes;
+  } else {
+    str += minutes;
+  }
+  return str;
 };
 
 origin = location.origin + "/gofish/api/";
