3ff3d78bf9f92772d7e7a6b00e3a1b9c50adab6c
Maksim Solovjov <himaxx2@gmail.com>
2014-03-17 12:27:55 +0000

    Change random generation of lakes to something more sensible

--- go_fish_project/gofish/generators.py
+++ go_fish_project/gofish/generators.py
@@ -7,72 +7,98 @@ worldHeight = 16
 
 def generateLake():
     lake = []
+    # fill in the lake with ones
     for i in range(0, worldHeight):
-    	lake.append([])
+        lake.append([])
+    for i in range(0, worldHeight):
+        for j in range(0, worldWidth):
+            lake[i].append(1)
 
-    for i in range(0, worldHeight):
-    	for j in range(0, worldWidth):
-    		lake[i].append(floor(randint(1,10)))
+    # make holes
+    for i in range(0, randint(0,5)):
+        makeHole(lake, worldWidth, worldHeight)
 
     return lake
 
-def generateWeather():
-	weatherSet = []
-	for i in range(12):
-		weatherList = {}
-		rain = 0
-		windms = -1
-		winddir = -1
-		temp = -1
-		
-		clouds = randint(0,2)
-		weatherList['clouds'] = clouds
-		
-		if clouds != 0:
-			rain = randint(1,2)
-		weatherList['rain'] = rain
-		
-		windms = gauss(8+rain+clouds,2+rain)
-		weatherList['windms'] = windms
-		
-		winddir = randint(0,3)
-		weatherList['winddir'] = winddir
-		
-		temp = randint(10,25)
-		weatherList['temp'] = temp
+def makeHole(lake, w, h):
+    # choose the pit
+    x = randint(0, w-1)
+    y = randint(0, h-1)
 
-		weatherSet.append(dict(weatherList))
-	return weatherSet
-	
+    # choose the depth
+    d = randint(3, 10)
+
+    # start spreading the hole
+    digDown(lake, d, x, y, w, h)
+
+def digDown(lake, d, x, y, w, h):
+    if x < 0 or y < 0 or x >= w or y >= h:
+        return
+    if lake[y][x] >= d:
+        return
+    lake[y][x] = d
+    digDown(lake, d-1, x, y-1, w, h)
+    digDown(lake, d-1, x+1, y, w, h)
+    digDown(lake, d-1, x, y+1, w, h)
+    digDown(lake, d-1, x-1, y, w, h)
+
+def generateWeather():
+    weatherSet = []
+    for i in range(12):
+        weatherList = {}
+        rain = 0
+        windms = -1
+        winddir = -1
+        temp = -1
+        
+        clouds = randint(0,2)
+        weatherList['clouds'] = clouds
+        
+        if clouds != 0:
+            rain = randint(1,2)
+        weatherList['rain'] = rain
+        
+        windms = gauss(8+rain+clouds,2+rain)
+        weatherList['windms'] = windms
+        
+        winddir = randint(0,3)
+        weatherList['winddir'] = winddir
+        
+        temp = randint(10,25)
+        weatherList['temp'] = temp
+
+        weatherSet.append(dict(weatherList))
+    return weatherSet
+    
 def perceivedDepth(hour, depth):
-	'''
-	6 7 8 9 10 11 12 13 14 15 16 17
-	0 1 2 3 4  5  6  7  8  9  10 11
-	0 1 2 3 4  5  6  5  4  3  2  1
-	'''
-	absHour = hour -6;
-	if absHour > 6:
-		absHour = absHour - (absHour - 6)
-	modDepth = depth - absHour/2
-	pDepth = -1
-	if modDepth >= 7:
-		pDepth = 3
-	elif modDepth >= 4:
-		pDepth = 2
-	elif modDepth >= 1:
-		pDepth = 1
-	else:
-		pDepth = 0
-	'''
-	6 >= 10 = 3
-	0 >= 7  = 3
-	6 >= 7  = 2
-	0 >= 4  = 2
-	6 >= 4  = 1
-	0 >= 1  = 1
-	6 >= 1  = 0
-	'''
-	return pDepth
+    '''
+    6 7 8 9 10 11 12 13 14 15 16 17
+    0 1 2 3 4  5  6  7  8  9  10 11
+    0 1 2 3 4  5  6  5  4  3  2  1
+    '''
+    absHour = hour -6;
+    if absHour > 6:
+        absHour = absHour - (absHour - 6)
+    modDepth = depth - absHour/2
+    pDepth = -1
+    if modDepth >= 7:
+        pDepth = 3
+    elif modDepth >= 4:
+        pDepth = 2
+    elif modDepth >= 1:
+        pDepth = 1
+    else:
+        pDepth = 0
+    '''
+    6 >= 10 = 3
+    0 >= 7  = 3
+    6 >= 7  = 2
+    0 >= 4  = 2
+    6 >= 4  = 1
+    0 >= 1  = 1
+    6 >= 1  = 0
+    '''
+    return pDepth
 
 def perceivedWindyness(windms):
     if windms < 5:
